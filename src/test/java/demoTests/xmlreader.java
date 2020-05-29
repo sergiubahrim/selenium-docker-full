bae1a47fefe6bb818c2e78041ea6175bebe7a69a
sergiubahrim <sergiu.bahrim@tradeshift.com>
2020-05-29 13:39:41 +0300

    adding api test jar files

--- src/test/java/demoTests/xmlreader.java
+++ src/test/java/demoTests/xmlreader.java
@@ -1,21 +1,13 @@
 package demoTests;
 
-import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.UUID;
 
 public class xmlreader {
 
-
-  /* public static String generateStringFromResource(String path) throws IOException {
-
-        return new String(Files.readAllBytes(Paths.get(path)));
-
-    }*/
     String uuid = UUID.randomUUID().toString();
     String tokenR = "nXbKRbQ5sX5n34zTDc5JSjzxfGJ2EE";
     String tokenRsecret = "YypVAtyWHgd@esPm7XDGUcKG3UgXsHvrCVRPuT8d";
@@ -27,7 +19,7 @@ public class xmlreader {
 
  //ClassLoader classloader = Thread.currentThread().getContextClassLoader();
  //InputStream myRequest = TradeshiftDocumentsAutomation.class.getResourceAsStream("/");
-    //File myRequest = new File("/Users/sergiu.bahrim/IdeaProjects/seleniumdockerComplete/src/main/resources/TestData");
+ //File myRequest = new File("/Users/sergiu.bahrim/IdeaProjects/seleniumdockerComplete/src/main/resources/TestData");
 
 
  public String readResource(final String fileName, Charset charset) throws IOException {
